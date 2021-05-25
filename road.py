94a2b70a5b311817abdee68e00eb695909cde116
Rinze Douma <50236992+therinz@users.noreply.github.com>
2021-05-25 13:42:54 +0200

    try CD pythonanywhere

---
+++ road.py
@@ -0,0 +1,8 @@
+
+import geopandas as gpd
+import fiona
+
+file = 'https://gist.githubusercontent.com/soobrosa/b3500853a0d4633fd963/raw/ecd9998983ec1dcad1b34d47c90871c9a9fc7e5d/sensor.kml'
+
+gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+df = gpd.read_file(file, driver='KML')
